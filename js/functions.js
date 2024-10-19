b30c513d1da2d575e14ad5fb86f3001403a6df72
Ксения <grigorevaksenis05@gmail.com>
2024-10-19 18:08:34 +0500

    module5-task2

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,22 @@
-function checkLengthString(string, maxLength) {
-  return string.length <= maxLength;
-}
-
-checkLengthString('проверяемая строка', 20); // true
-checkLengthString('проверяемая строка', 18); // true
-checkLengthString('проверяемая строка', 10); // false
-
-function isPalindrome(str) {
-  const normalizedStr = str.replaceAll(' ', '').toLowerCase();
-  let reversedStr = '';
-
-  for (let i = normalizedStr.length - 1; i >= 0; i--) {
-    reversedStr += normalizedStr[i];
+function isMeetingWithinWorkDay(workDayStart, workDayEnd, meetingStart, meetingDuration) {
+  // Преобразуем время в минуты
+  function timeToMinutes(time) {
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
   }
 
-  return reversedStr === normalizedStr;
+  const workDayStartMinutes = timeToMinutes(workDayStart);
+  const workDayEndMinutes = timeToMinutes(workDayEnd);
+
+  const meetingStartMinutes = timeToMinutes(meetingStart);
+
+  const meetingEndMinutes = meetingStartMinutes + meetingDuration;
+
+  return meetingStartMinutes >= workDayStartMinutes && meetingEndMinutes <= workDayEndMinutes;
 }
 
-isPalindrome('Лёша на полке клопа нашёл ');
-isPalindrome('довод');
-
-
+isMeetingWithinWorkDay('08:00', '17:30', '14:00', 90);
+isMeetingWithinWorkDay('8:0', '10:0', '8:0', 120);
+isMeetingWithinWorkDay('08:00', '14:30', '14:00', 90);
+isMeetingWithinWorkDay('14:00', '17:30', '08:0', 90);
+isMeetingWithinWorkDay('8:00', '17:30', '08:00', 900);
