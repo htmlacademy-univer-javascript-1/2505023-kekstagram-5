2de11b53bba3a19cf7ada0c0846a056903fa8ea4
Ксения <grigorevaksenis05@gmail.com>
2024-12-13 12:50:42 +0500

    module11-task1

--- js/pictures.js
+++ js/pictures.js
@@ -1,4 +1,3 @@
-import {objects} from './data.js';
 import {openBigPicture} from './big-picture.js';
 
 const pattern = document.querySelector('#picture').content.querySelector('.picture');
@@ -27,4 +26,4 @@ const renderPictures = (pictures) => {
   container.append(fragment);
 };
 
-renderPictures(objects);
+export { renderPictures };
